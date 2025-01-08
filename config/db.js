20c2bac8b291873993f68b7d306880f1ab348e91
rldarshan <rl.darshan01@gmail.com>
2025-01-08 15:02:45 +0530

    updated package

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const logger = require('./logger');
 
 const connectDB = async (uri) => {
     try {
-        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(uri
+        //     , {
+        //     dbName: process.env.DB_NAME,
+        //   }
+        );
         logger.info('MongoDB connected');
     } catch (err) {
         logger.error('MongoDB connection error:', err);
